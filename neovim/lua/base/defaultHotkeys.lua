d5bec636918323b73ce61244b7340b96be857f08
Rustam <hrustbas@gmail.com>
2024-05-09 22:30:15 +0300

    make hotkeys for split

--- neovim/lua/base/defaultHotkeys.lua
+++ neovim/lua/base/defaultHotkeys.lua
@@ -3,3 +3,7 @@ vim.keymap.set('n','<leader>h',':nohlsearch<CR>')
 
 -- make 
 vim.keymap.set('n','<leader>m',':make<CR>')
+
+-- Splits
+vim.keymap.set('n','<leader>sh',':split h<CR>')
+vim.keymap.set('n','<leader>sv',':split v<CR>')
