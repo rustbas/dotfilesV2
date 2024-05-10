e10c3029325c9d1d4f549b33558ae029c35e0dca
Rustam <hrustbas@gmail.com>
2024-05-10 11:38:40 +0300

    add quickfix list hotkeys
    Group quickfix hotkeys started with `q`

--- neovim/lua/base/defaultHotkeys.lua
+++ neovim/lua/base/defaultHotkeys.lua
@@ -7,3 +7,8 @@ vim.keymap.set('n','<leader>m',':make<CR>')
 -- Splits
 vim.keymap.set('n','<leader>sh',':split h<CR>')
 vim.keymap.set('n','<leader>sv',':split v<CR>')
+
+-- QuickFix list (Maybe must add some hotkeys)
+vim.keymap.set('n','<leader>qo',':copen<CR>')
+vim.keymap.set('n','<leader>qp',':cprev<CR>')
+vim.keymap.set('n','<leader>qn',':cnext<CR>')
