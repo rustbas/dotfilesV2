073497a4f5f841946f352ce3164d4d717adafbd8
Basyrov Rustam <bronit10@morozko.ru>
2024-05-18 14:15:18 +0300

    create push/pull hotkeys

--- neovim/lua/base/plugins.lua
+++ neovim/lua/base/plugins.lua
@@ -39,7 +39,8 @@ vim.keymap.set('n','<leader>n',':NERDTreeToggle<CR>')
 
 -- Vim-fugitive
 vim.keymap.set('n','<leader>gg',':Git<CR>')
-vim.keymap.set('n','<leader>gp',':Git push<CR>')
+vim.keymap.set('n','<leader>gP',':Git push<CR>')
+vim.keymap.set('n','<leader>gp',':Git pull --no-rebase<CR>')
 -- vim.keymap.set('n','<leader>gcb',':Git checkout ')
 
 -- Telescope
