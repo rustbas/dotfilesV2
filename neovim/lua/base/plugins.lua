c5afb08cf4332180e466fc74ba33cee44fa7f952
Basyrov Rustam <bronit10@morozko.ru>
2024-05-18 16:25:50 +0300

    turn off folding for telescope buffers

--- neovim/lua/base/plugins.lua
+++ neovim/lua/base/plugins.lua
@@ -49,3 +49,10 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = {"TelescopeResults"},
+    callback = function()
+        vim.opt.nofoldenable = true
+    end
+})
