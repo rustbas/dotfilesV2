d44efc8aa613a65ded836def77b0be40e6c86f79
Rustam Basyrov <hrustbas@gmail.com>
2024-08-04 15:33:13 +0300

    install zsh config

---
+++ install.sh
@@ -0,0 +1,29 @@
+#############
+# Variables #
+#############
+
+# I don't like it, but can't do better
+DOTFILES="/home/$USER/ws/dotfilesV2"
+
+XDG_CONFIG_HOME=$HOME/.config 
+XDG_CACHE_HOME=$HOME/.cache 
+XDG_DATA_HOME=$HOME/.local/share 
+XDG_STATE_HOME=$HOME/.local/state 
+
+
+#############
+# Setup zsh #
+#############
+
+# Home env
+( [[ -L ~/.zshenv ]] || [[ -f ~/.zshenv ]] ) && rm -f ~/.zshenv
+ln -s "$DOTFILES/zsh/.home.zshenv" ~/.zshenv
+
+# ZDOTDIR
+[[ -d $XDG_CONFIG_HOME/zsh ]] && rm -rf $XDG_CONFIG_HOME/zsh
+mkdir --parents $XDG_CONFIG_HOME/zsh
+ln -s "$DOTFILES/zsh/.zshenv" $XDG_CONFIG_HOME/zsh
+ln -s "$DOTFILES/zsh/.zshrc" $XDG_CONFIG_HOME/zsh
+
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
