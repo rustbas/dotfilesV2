30db4780c1642cd1b563fa78e9ee6945e43482fe
Rustam Basyrov <hrustbas@gmail.com>
2024-08-04 16:01:07 +0300

    some fixes

--- install.sh
+++ install.sh
@@ -18,6 +18,13 @@ XDG_STATE_HOME=$HOME/.local/state
 [[ -L $XDG_CONFIG_HOME/tmux ]] && rm -f $XDG_CONFIG_HOME/tmux
 ln -s "$DOTFILES/tmux" $XDG_CONFIG_HOME/tmux
 
+################
+# Setup neovim #
+################
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+[[ -L $XDG_CONFIG_HOME/nvim ]] && rm -f $XDG_CONFIG_HOME/nvim
+ln -s "$DOTFILES/neovim" $XDG_CONFIG_HOME/nvim
+
 ############################
 # Setup zsh (must be last) #
 ############################
@@ -30,7 +37,28 @@ ln -s "$DOTFILES/zsh/.home.zshenv" ~/.zshenv
 [[ -d $XDG_CONFIG_HOME/zsh ]] && rm -rf $XDG_CONFIG_HOME/zsh
 mkdir --parents $XDG_CONFIG_HOME/zsh
 ln -s "$DOTFILES/zsh/.zshenv" $XDG_CONFIG_HOME/zsh
-ln -s "$DOTFILES/zsh/.zshrc" $XDG_CONFIG_HOME/zsh
+# ln -s "$DOTFILES/zsh/.zshrc" $XDG_CONFIG_HOME/zsh
+touch $XDG_CONFIG_HOME/.zshrc
 
 # oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &
+sleep 5
+
+# some settings
+cat >> $XDG_CONFIG_HOME/zsh/.zshrc << EOF
+#!/bin/zsh
+
+# Completion
+autoload -U compinit
+compinit
+ 
+# Correction
+setopt correctall
+  
+# Prompt
+# autoload -U promptinit
+# promptinit
+# prompt fire
+EOF
+
+
