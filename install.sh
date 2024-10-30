a01ca49d63cc6950a68ba9852661154cac43ea20
Basyrov Rustam <hrustbas@gmail.com>
2024-10-30 15:01:28 +0300

    remove auto install vim-plug

--- install.sh
+++ install.sh
@@ -58,7 +58,7 @@ ln -s "$DOTFILES/i3/config" $XDG_CONFIG_HOME/i3/config
 ################
 # Setup neovim #
 ################
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+# sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 [[ -L $XDG_CONFIG_HOME/nvim ]] && rm -f $XDG_CONFIG_HOME/nvim
 ln -s "$DOTFILES/neovim" $XDG_CONFIG_HOME/nvim
 
