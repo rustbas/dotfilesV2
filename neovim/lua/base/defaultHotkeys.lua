60157bb1017048b9173d90b23e5eab31bcf1d5f2
Basyrov Rustam <hrustbas@gmail.com>
2024-11-01 10:30:15 +0300

    fix vsplit

--- neovim/lua/base/defaultHotkeys.lua
+++ neovim/lua/base/defaultHotkeys.lua
@@ -6,8 +6,8 @@ vim.keymap.set('n','<leader>mm',':make<CR>')
 vim.keymap.set('n','<leader>mc',':make clean<CR>')
 
 -- Splits
-vim.keymap.set('n','<leader>sh',':split h<CR>')
-vim.keymap.set('n','<leader>sv',':split v<CR>')
+vim.keymap.set('n','<leader>sh',':split<CR>')
+vim.keymap.set('n','<leader>sv',':vsplit<CR>')
 
 -- QuickFix list (Maybe must add some hotkeys)
 vim.keymap.set('n','<leader>qo',':copen<CR>')
