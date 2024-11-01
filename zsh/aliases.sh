9d2a6ceb1315069213b03c57d7f76065b8604034
Basyrov Rustam <hrustbas@gmail.com>
2024-11-01 22:45:47 +0300

    add restore file function

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -7,3 +7,20 @@ alias gb="git branch"
 alias gc="git commit"
 alias gd="git diff"
 alias gp="git push"
+
+# Useful aliases
+
+# restore .bak file to current dir
+restore-file() {
+    backup_dir=$(realpath $(dirname $1))
+    backup_file=$(basename $1)
+    
+    restored_file=${backup_file%*.bak}
+    restored_dir=$(realpath .)
+
+    echo "Restoring $backup_file 
+    FROM: $backup_dir 
+    TO:   $restored_dir"
+
+    rsync --info=progress2 "$backup_dir/$backup_file" "$restored_dir/$restored_file"
+}
