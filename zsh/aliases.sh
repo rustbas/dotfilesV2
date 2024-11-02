07971993ffb9feb29673ace9c22ebb8b734c54f6
Basyrov Rustam <hrustbas@gmail.com>
2024-11-02 08:44:40 +0300

    some lf utilities
    - fzf_search
    - follow_link
    - bulk-rename
    - copy paths
    - quit-on-cd

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -25,3 +25,18 @@ restore-file() {
     rsync --info=progress2 "$backup_dir/$backup_file" "$restored_dir/$restored_file"
 }
 alias rf=restore-file
+
+# cd to dir on quit in lf
+lf() {
+    export LF_CD_FILE="/var/tmp/.lfcd-$$"
+    command lf "$@"
+    if [ -s "$LF_CD_FILE" ]; then
+        local DIR="$(realpath -- "$(cat -- "$LF_CD_FILE")")"
+        if [ "$DIR" != "$PWD" ]; then
+            printf 'cd to %s\n' "$DIR"
+            cd "$DIR"
+        fi
+        rm "$LF_CD_FILE"
+    fi
+    unset LF_CD_FILE
+}
