8eff68617f8434e2570175f8139da12159c326c7
Basyrov Rustam <hrustbas@gmail.com>
2025-03-21 08:49:06 +0300

    fix howm-files

--- emacs/additional-files/packages.el
+++ emacs/additional-files/packages.el
@@ -16,8 +16,8 @@
   :init
   (setq howm-directory "~/ws/docs/NotesOrgMode/howm/")
   ;; (setq howm-directory "~/howm/")
-  (setq howm-keyword-file "~/ws/docs/NotesOrgMode/howm/")
-  (setq howm-history-file "~/ws/docs/NotesOrgMode/howm/")
+  (setq howm-keyword-file "~/ws/docs/NotesOrgMode/howm/howm-keys")
+  (setq howm-history-file "~/ws/docs/NotesOrgMode/howm/howm-history")
   (setq howm-file-name-format "%Y/%m/%Y-%m-%d-%H%M%S.org")
   ;; (require 'howm-org) ;; Write notes in Org-mode.
   (setq howm-follow-theme t) ;; Use your Emacs theme colors.
